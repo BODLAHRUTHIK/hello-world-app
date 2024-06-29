f92f79e9c3da44ec6346edbed63c6d6c4da246fc
bodla hruthik <bodlahruthik18@gmail.com>
2024-06-30 02:47:29 +0530

    added project

---
+++ project-flask/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+
+def hello():
+    if request.method == 'GET':
+        d = {'data': "hello world"}
+        return jsonify(d)
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6123)
