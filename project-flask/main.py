c87a567e8d8b44d786cd6120c740b0e4eeab0ac3
BODLAHRUTHIK <66299878+BODLAHRUTHIK@users.noreply.github.com>
2024-07-02 17:36:43 +0530

    Update main.py

--- project-flask/main.py
+++ project-flask/main.py
@@ -10,7 +10,6 @@ def hello():
         d = {'data': "hello world"}
         return jsonify(d)
     
-#This is commmen
-
+print('test')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6123)
