4468d693e9446e42a6219d259bae4fa7e632164e
BODLAHRUTHIK <66299878+BODLAHRUTHIK@users.noreply.github.com>
2024-07-17 19:16:41 +0530

    Update main.py

--- project-flask/main.py
+++ project-flask/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def hello():
     if request.method == 'GET':
-        d = {'data': "Hello!. pipeline is working. checking again"}
+        d = {'data': "Hello!. pipeline is working"}
         return jsonify(d)
 
 if __name__ == '__main__':
